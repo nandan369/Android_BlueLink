cabc51d4547b7001b29332b563ea0dfe17b2f37b
Alexandre Maule <alexpmaule@gmail.com>
2025-07-25 16:24:55 -0500

    Added DeviceDetailActivity class with it's layout activity_device_detail

--- app/src/main/java/com/example/bluelink/activities/DeviceDetailActivity.kt
+++ app/src/main/java/com/example/bluelink/activities/DeviceDetailActivity.kt
@@ -6,9 +6,9 @@ import com.example.bluelink.databinding.ActivityDeviceDetailBinding
 
 class DeviceDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDeviceDetailBinding
-    private lateinit var deviceName: String
-    private lateinit var deviceAddress: String
-    private var rssi: Int = 0
+    private var deviceName: String? = null
+    private var deviceAddress: String? = null
+    private var rssi: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +18,25 @@ class DeviceDetailActivity : AppCompatActivity() {
         deviceName = intent.getStringExtra("deviceName") ?: "Unknown Device"
         deviceAddress = intent.getStringExtra("deviceAddress") ?: "Unknown Address"
         rssi = intent.getIntExtra("rssi", 0)
-        val rssiText = "RSSI: $rssi dBm"
 
-        binding.ddDeviceName.text = deviceName
-        binding.ddDeviceAddress.text = deviceAddress
-        binding.ddRssi.text = rssiText
+        loadDeviceDetails(deviceName, deviceAddress, rssi)
 
         binding.button.setOnClickListener {
             // Go to Log History Activity
         }
 
+
     }
+
+    fun loadDeviceDetails(deviceName: String?, deviceAddress: String?, rssi: Int?) {
+        binding.ddDeviceName.text = deviceName?: "Unknown Device"
+        binding.ddDeviceAddress.text = deviceAddress?: "Unknown Address"
+        if (rssi != null) {
+            val rssiText = "RSSI: $rssi dBm"
+            binding.ddRssi.text = rssiText
+        } else {
+            binding.ddRssi.text = "RSSI: Unknown"
+        }
+    }
+
 }
