143d325ba2bbf0455ceb840480865f72be5e56a4
Alexandre Maule <alexpmaule@gmail.com>
2025-08-12 09:21:22 -0500

    Created a GattHolder class for the desired BluetoothGatt connected BluetoothGattServices to the adapter.

--- app/src/main/java/com/example/bluelink/adapters/DeviceServicesAdapter.kt
+++ app/src/main/java/com/example/bluelink/adapters/DeviceServicesAdapter.kt
@@ -1,57 +1,170 @@
 package com.example.bluelink.adapters
 
+import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothGattService
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
-import com.example.bluelink.R
-import com.example.bluelink.databinding.ActivityDeviceDetailBinding
+import com.example.bluelink.databinding.ItemDeviceCharacteristicBinding
 import com.example.bluelink.databinding.ItemDeviceServiceBinding
 
-class DeviceServicesAdapter(private val serviceList: List<BluetoothGattService>) : RecyclerView.Adapter<DeviceServicesAdapter.ViewHolder>() {
+class DeviceServicesAdapter(
+    private val itemList: List<Any>,
+    private val characteristicListener: CharacteristicActionListener?
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    class ViewHolder(val binding: ItemDeviceServiceBinding): RecyclerView.ViewHolder(binding.root)
+    private val VIEW_TYPE_SERVICE = 1
+    private val VIEW_TYPE_CHARACTERISTIC = 2
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemDeviceServiceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    // ViewHolder for services
+    class ServiceViewHolder(val binding: ItemDeviceServiceBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(service: BluetoothGattService) {
+            val uuid = service.uuid.toString().substring(4, 8)
+            binding.itemDeviceServiceName.text = uuidToName(uuid)
+            binding.itemDeviceServiceUuid.text = uuid
+            binding.itemDeviceServiceType.text = service.type.toString()
+        }
+    }
+
+    // ViewHolder for characteristics
+    class CharacteristicViewHolder(val binding: ItemDeviceCharacteristicBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            characteristic: BluetoothGattCharacteristic,
+            listener: CharacteristicActionListener?
+        ) {
+            val uuid = characteristic.uuid.toString().substring(4, 8)
+            binding.tvCharacteristicName.text = uuidToName(uuid)
+            binding.tvCharacteristicUuidNumber.text = uuid
+            binding.tvCharacteristicProperties.text = characteristic.properties.toString()
+
+            // Set visibility of buttons based on characteristic properties
+            val properties = characteristic.properties
+            if (properties and BluetoothGattCharacteristic.PROPERTY_READ > 0) {
+                binding.characteristicReadButton.isVisible = true
+                binding.characteristicReadButton.setOnClickListener {
+                    // Handle read button click
+                    // Show a dialog with the read value
+                    // Save the read value to SharedPreferences
+                    listener?.onReadClicked(characteristic)
+                }
+            }
+
+            if (properties and BluetoothGattCharacteristic.PROPERTY_WRITE > 0) {
+                binding.characteristicWriteButton.isVisible = true
+                binding.characteristicWriteButton.setOnClickListener {
+                    // Handle write button click
+                    // Show a dialog with the write value
+                    val inputEditText = EditText(it.context)
+                    val alert = AlertDialog.Builder(it.context)
+                        .setTitle("Write Value")
+                        .setMessage("Enter the value to write: ")
+                        .setView(inputEditText)
+                        .setPositiveButton("Write") { _, _ ->
+                            // Handle OK button click
+                            val value = inputEditText.text.toString()
+                            if (value.isNotEmpty()) {
+                                listener?.onWriteClicked(characteristic,value.toByteArray())
+                            }
+                        }
+                        .setNegativeButton("Cancel", null)
+                        .create()
+                    alert.show()
+                }
+            }
+
+            if (properties and BluetoothGattCharacteristic.PROPERTY_NOTIFY > 0) {
+                binding.characteristicNotifyButton.isVisible = true
+                binding.characteristicNotifyButton.setOnClickListener {
+                    // Handle notify button click
+                    // Show a dialog with the notify value
+                    val options = arrayOf("Enable", "Disable")
+                    var selectedOption = 0
+                    val alert = AlertDialog.Builder(it.context)
+                        .setTitle("Notify")
+                        .setSingleChoiceItems(options, selectedOption) { _, which -> selectedOption = which }
+                        .setPositiveButton("Apply") { _, _ ->
+                            // Handle OK button click
+                            val enable = selectedOption == 0
+                            listener?.onNotifyClicked(characteristic, enable)
+                        }
+                        .setNegativeButton("Cancel", null)
+                        .create()
+                    alert.show()
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        // return the appropriate ViewHolder based on the view type
+        return when (viewType) {
+            VIEW_TYPE_SERVICE -> {
+                val binding = ItemDeviceServiceBinding.inflate(inflater, parent, false)
+                ServiceViewHolder(binding)
+            }
+
+            VIEW_TYPE_CHARACTERISTIC -> {
+                val binding = ItemDeviceCharacteristicBinding.inflate(inflater, parent, false)
+                CharacteristicViewHolder(binding)
+            }
+
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
     override fun onBindViewHolder(
-        holder: ViewHolder,
+        holder: RecyclerView.ViewHolder,
         position: Int
     ) {
-        val service = serviceList[position]
-        with(holder.binding) {
-            itemDeviceServiceName.text = uuidToName(service.uuid.toString())
-            btnRead.setOnClickListener {
-                // Handle read button click
-            }
-            btnWrite.setOnClickListener {
-                // Handle write button click
-            }
-            btnNotify.setOnClickListener {
-                // Handle notify button click
+        val item = itemList[position]
+        when (holder) {
+            is ServiceViewHolder -> holder.bind(item as BluetoothGattService)
+            is CharacteristicViewHolder -> holder.bind(
+                item as BluetoothGattCharacteristic,
+                characteristicListener
+            )
+
+            else -> throw IllegalArgumentException("Invalid view holder type")
+        }
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    override fun getItemViewType(position: Int): Int {
+        return when (itemList[position]) {
+            is BluetoothGattService -> VIEW_TYPE_SERVICE
+            is BluetoothGattCharacteristic -> VIEW_TYPE_CHARACTERISTIC
+            else -> throw IllegalArgumentException("Invalid item type")
+        }
+    }
+
+    companion object {
+        fun uuidToName(uuid: String): String {
+            return when (uuid) {
+                "1800" -> "Generic Access"
+                "1801" -> "Generic Attribute"
+                "180a" -> "Device Information"
+                "180f" -> "Battery Service"
+                "180d" -> "Heart Rate"
+                "1810" -> "Blood Pressure"
+                "1811" -> "Alert Notification"
+                "1812" -> "Running Speed and Cadence"
+                "1813" -> "Pulse Oximeter"
+                "1814" -> "Pulse Rate"
+                else -> "Unknown Service"
             }
         }
     }
 
-    override fun getItemCount(): Int = serviceList.size
-
-    fun uuidToName(uuid: String): String {
-        return when (uuid) {
-            "00001800-0000-1000-8000-00805f9b34fb" -> "Generic Access"
-            "00001801-0000-1000-8000-00805f9b34fb" -> "Generic Attribute"
-            "0000180a-0000-1000-8000-00805f9b34fb" -> "Device Information"
-            "0000180f-0000-1000-8000-00805f9b34fb" -> "Battery Service"
-            "00001810-0000-1000-8000-00805f9b34fb" -> "Heart Rate"
-            "00001811-0000-1000-8000-00805f9b34fb" -> "Device Information"
-            "00001812-0000-1000-8000-00805f9b34fb" -> "Running Speed and Cadence"
-            "00001813-0000-1000-8000-00805f9b34fb" -> "Pulse Oximeter"
-            "00001814-0000-1000-8000-00805f9b34fb" -> "Pulse Rate"
-            else -> "Unknown Service"
-        }
+    interface CharacteristicActionListener {
+        fun onReadClicked(characteristic: BluetoothGattCharacteristic)
+        fun onWriteClicked(characteristic: BluetoothGattCharacteristic, value: ByteArray)
+        fun onNotifyClicked(characteristic: BluetoothGattCharacteristic, enable: Boolean)
     }
 }
